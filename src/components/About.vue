0ebdfb54f3725eefabfcf52d4b794b236791cd42
Carlos Mañas <carlos@spacenomads.com>
2019-12-09 13:33:55 +0100

    My first Vue App

---
+++ src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>Hello {{their_name}}, this an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+      their_name: this.$route.params.name
+    }
+  }
+}
+</script>
+
---
+++ src/components/Skills.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="hello">
+    <div class="holder">
+      <form @submit.prevent="addSkill">
+        <input
+          type="text"
+          placeholder="Enter a skill you have…"
+          v-model="skill"
+          v-validate="'min:5'"
+          name="skill"
+        >
+        <transition
+          name="alert-in"
+          enter-active-class="animated flipInX"
+          enter-leave-class="animated flipOutX"
+        >
+          <p class="alert" v-if="errors.has('skill')">{{errors.first('skill')}}</p>
+        </transition>
+      </form>
+      <ul class="skills">
+        <transition-group
+          name="list "
+          enter-active-class="animated bounceInUp"
+          enter-leave-class="animated BounceOutDown"
+        >
+          <li class="skill" v-for="(data, index) in skills" :key='index'>
+            {{data.skill}}
+            <i class="fa fa-minus-circle" v-on:click="remove(index)"></i>
+
+          </li>
+        </transition-group>
+      </ul>
+      <p>These are your skills</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Skills',
+  data() {
+    return {
+      skill: '',
+      skills: [
+        {skill: 'Vue.js'},
+        {skill: 'Napalm'}
+      ]
+    }
+  },
+  methods: {
+    addSkill() {
+      this.$validator.validateAll().then(result => {
+        if (result) {
+          this.skills.push({skill: this.skill});
+          this.skill = '';
+        } else {
+          // eslint-disable-next-line
+          console.log('Not valid');
+        }
+      });
+    },
+    remove(id) {
+      this.skills.splice(id,1);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  @import 'https://cdn.jsdelivr.net/npm/animate.css@3.5.1';
+  @import 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css';
+  .holder {
+    background: #fff;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+  }
+
+  p {
+    text-align:center;
+    padding: 30px 0;
+    color: gray;
+  }
+
+  .container {
+    box-shadow: 0px 0px 40px lightgray;
+  }
+  input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #323333;
+    color: #687F7F;
+  }
+  .alert {
+    background: #fdf2ce;
+    font-weight: bold;
+    display: inline-block;
+    padding: 5px;
+    margin-top: -20px;
+  }
+  .alert-in-enter-active {
+    animation: bounce-in .5s;
+  }
+  .alert-in-leve-active {
+    animation: bounce-in .5s reverse;
+  }
+
+  @keyframes bounce-in {
+    0% {
+      transform: scale(0);
+    }
+    50% {
+      transform: scale(1.5);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+  i {
+    float:right;
+    cursor: pointer;
+  }
+</style>
